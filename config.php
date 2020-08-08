96ef4dcf5d9fa48db52190d02a596598b1c9ca34
muliady24 <47475991+muliady24@users.noreply.github.com>
2020-08-08 14:53:38 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,220 @@
+<?php
+// -*- Copyleft @ °✍⎯⚚͜͡SʰⁱNᵒᵇⁱ - 2020 -*-
+// -*- Config Fast New -*-
+// -*- If you want to donate, at a DOGE Wallet:
+// -*- DDsu3tCfqXbhoj1j84Ydq3qkBYNZZESC3Q
+//________________________________________________
+
+$wallet = "EC-UserId-62010"; //Your EC-ID
+
+// Example web type!
+// https://fastcoins.tk/?r=EC-UserId-62010
+// https://24.sharelink.club/?r=EC-UserId-62010
+// cari data config dari jenis web yg sama dengan web di atas!
+
+$url1 = "https://fastcoins.tk/claim.php?coin=DOGE";
+$sid1 = "PHPSESSID=67c9cf85de29ce8f803c3b64df7ef842";
+$tok1 = "DOGEToken=1QJvYOHOQ7tod9FDrYus5LGKFKbii7ym";
+
+$url2 = "https://autofaucets.org/ltc24/claim.php?coin=LTC";
+$sid2 = "PHPSESSID=052d6a1683c3864560528b1ce471d58f";
+$tok2 = "LTCToken=eeC2TAPzl7yCpBD4AtmbiT5IRjmjqYoD";
+
+$url3 = "https://24.sharelink.club/claim.php?coin=EXS";
+$sid3 = "PHPSESSID=06e6bd1d23f38c2d600df7fa90de7c23";
+$tok3 = "EXSToken=Glp7oZhA7ERW5EKN8AEHSDlBeePkJHlH";
+
+$url4 = "https://starfaucet.net/Autofaucet/claim.php?coin=ETC";
+$sid4 = "PHPSESSID=7becef5f49569c2c64aaabf65216a825";
+$tok4 = "ETCToken=r8ysk6lblE2ybxdyUIRq5rLB6kPnnfqp";
+
+$url5 = "";
+$sid5 = "";
+$tok5 = "";
+
+$url6 = "";
+$sid6 = "";
+$tok6 = "";
+
+$url7 = "";
+$sid7 = "";
+$tok7 = "";
+
+$url8 = "";
+$sid8 = "";
+$tok8 = "";
+
+$url9 = "";
+$sid9 = "";
+$tok9 = "";
+
+$url10 = "";
+$sid10 = "";
+$tok10 = "";
+
+$url11 = "";
+$sid11 = "";
+$tok11 = "";
+
+$url12 = "";
+$sid12 = "";
+$tok12 = "";
+
+$url13 = "";
+$sid13 = "";
+$tok13 = "";
+
+$url14 = "";
+$sid14 = "";
+$tok14 = "";
+
+$url15 = "";
+$sid15 = "";
+$tok15 = "";
+
+$url16 = "";
+$sid16 = "";
+$tok16 = "";
+
+$url17 = "";
+$sid17 = "";
+$tok17 = "";
+
+$url18 = "";
+$sid18 = "";
+$tok18 = "";
+
+$url19 = "";
+$sid19 = "";
+$tok19 = "";
+
+$url20 = "";
+$sid20 = "";
+$tok20 = "";
+
+$url21 = "";
+$sid21 = "";
+$tok21 = "";
+
+$url22 = "";
+$sid22 = "";
+$tok22 = "";
+
+$url23 = "";
+$sid23 = "";
+$tok23 = "";
+
+$url24 = "";
+$sid24 = "";
+$tok24 = "";
+
+$url25 = "";
+$sid25 = "";
+$tok25 = "";
+
+$url26 = "";
+$sid26 = "";
+$tok26 = "";
+
+$url27 = "";
+$sid27 = "";
+$tok27 = "";
+
+$url28 = "";
+$sid28 = "";
+$tok28 = "";
+
+$url29 = "";
+$sid29 = "";
+$tok29 = "";
+
+$url30 = "";
+$sid30 = "";
+$tok30 = "";
+
+$url31 = "";
+$sid31 = "";
+$tok31 = "";
+
+$url32 = "";
+$sid32 = "";
+$tok32 = "";
+
+$url33 = "";
+$sid33 = "";
+$tok33 = "";
+
+$url34 = "";
+$sid34 = "";
+$tok34 = "";
+
+$url35 = "";
+$sid35 = "";
+$tok35 = "";
+
+$url36 = "";
+$sid36 = "";
+$tok36 = "";
+
+$url37 = "";
+$sid37 = "";
+$tok37 = "";
+
+$url38 = "";
+$sid38 = "";
+$tok38 = "";
+
+$url39 = "";
+$sid39 = "";
+$tok39 = "";
+
+$url40 = "";
+$sid40 = "";
+$tok40 = "";
+
+$url41 = "";
+$sid41 = "";
+$tok41 = "";
+
+$url42 = "";
+$sid42 = "";
+$tok42 = "";
+
+$url43 = "";
+$sid43 = "";
+$tok43 = "";
+
+$url44 = "";
+$sid44 = "";
+$tok44 = "";
+
+$url45 = "";
+$sid45 = "";
+$tok45 = "";
+
+$url46 = "";
+$sid46 = "";
+$tok46 = "";
+
+$url47 = "";
+$sid47 = "";
+$tok47 = "";
+
+$url48 = "";
+$sid48 = "";
+$tok48 = "";
+
+$url49 = "";
+$sid49 = "";
+$tok49 = "";
+
+$url50 = "";
+$sid50 = "";
+$tok50 = "";
+
+// 50 Slot config Max!
+//Gunakan http cannary apk untuk mencari data di atas
+
+
+
+?>
